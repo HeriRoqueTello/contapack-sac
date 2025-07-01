87f7712ca89bddd090986fccebd31137bf4a732b
Heri Roque <heriroque.22@gmail.com>
2025-07-01 13:34:45 -0500

    feat: implement DashboardView layout with SectionCards and ChartAreaInteractive components

--- src/views/DashboardView.jsx
+++ src/views/DashboardView.jsx
@@ -1,3 +1,14 @@
+import { ChartAreaInteractive } from "@/components/admin/dashboard/chart-area-interactive";
+import { SectionCards } from "@/components/admin/dashboard/section-card";
+
 export function DashboardView() {
-  return <div>DashboardView</div>;
+  return (
+    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+      <SectionCards />
+      <div className="px-4 lg:px-6">
+        <ChartAreaInteractive />
+      </div>
+      {/* <DataTable data={data} /> */}
+    </div>
+  );
 }
