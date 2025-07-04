30863c067e50cff53eae795801c4be8911a2fa67
anthony <anthony.diaz.yacsahuanga@gmail.com>
2025-07-04 00:16:12 -0500

    feat: add dialog component and form handling with react-hook-form; update TaEtiquetaView to include new dialog

--- src/views/TaEtiquetaView.jsx
+++ src/views/TaEtiquetaView.jsx
@@ -1,19 +1,17 @@
-import { TableEtiqueta } from "@/components/ui/taetiqueta";   
+import { TableEtiqueta } from "@/components/admin/etiqueta/taetiqueta";
 import { ChartAreaInteractive } from "@/components/admin/dashboard/chart-area-interactive";
 import { SectionCards } from "@/components/admin/dashboard/section-card";
 import { AdminLayout } from "@/layout/AdminLayout";
 import { ProductCard } from "@/components/home/ui/ProductCard";
+import { DialogDemo } from "@/components/admin/etiqueta/dialogDemo";
 
 export function TaEtiquetaView() {
-
   return (
     <>
+      <div className="text-end">
+        <DialogDemo />
+      </div>
       <TableEtiqueta />
     </>
-    
-
-    
-    );
-
-
+  );
 }
