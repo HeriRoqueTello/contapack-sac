42b75829518a0b6cdc47a57918ffe973a062cf54
Heri Roque <heriroque.22@gmail.com>
2025-07-04 19:08:54 -0500

    fix: update CORS whitelist to include production frontend URL

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
 
-    const whiteList = [process.env.FRONTEND_URL, undefined]
+    const whiteList = [process.env.FRONTEND_URL, 'https://contapack-sac.vercel.app/', undefined]
 
     if (process.argv[2] === '--api') {
       whiteList.push(undefined)
