caa1afe99deb5c68980a41fdd34b3e36102b3284
Heri Roque <heriroque.22@gmail.com>
2025-07-04 19:14:45 -0500

    fix: enhance CORS logging for better debugging

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -3,7 +3,15 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
 
+    console.log('--- Depuración CORS Detallada ---');
+    console.log('Petición entrante desde el ORIGIN:', origin); // ESTO ES CLAVE
+    console.log('Valor de process.env.FRONTEND_URL:', process.env.FRONTEND_URL);
+
     const whiteList = [process.env.FRONTEND_URL, 'https://contapack-sac.vercel.app/', undefined]
+
+    console.log('Lista blanca configurada (Array):', whiteList);
+    console.log('¿Origin está incluido en la lista blanca?', whiteList.includes(origin));
+    console.log('------------------------------------');
 
     if (process.argv[2] === '--api') {
       whiteList.push(undefined)
