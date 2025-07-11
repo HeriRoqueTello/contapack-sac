f63f6d50e52cfc545a64f65d2ea65f1b57826c60
Heri Roque <heriroque.22@gmail.com>
2025-07-11 12:07:30 -0500

    refactor: migrate from TypeORM to Sequelize for database connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,30 +1,9 @@
-import { DataSource } from "typeorm";
-import { Usuario } from "../models/Usuario";
-import { Area } from "../models/Area";
-import { Rol } from "../models/Rol";
+import { Sequelize } from "sequelize-typescript";
+import dotenv from 'dotenv';
+dotenv.config()
 
-export const dataSource = new DataSource({
-  type: 'mysql',
-  host: process.env.DB_HOST || 'localhost', // Tu host de MySQL
-  port: parseInt(process.env.DB_PORT || '3306', 10), // Tu puerto de MySQL
-  username: process.env.DB_USER || 'root', // Tu usuario de MySQL
-  password: process.env.DB_PASSWORD || 'password', // Tu contraseña de MySQL
-  database: process.env.DB_NAME || 'nombre_de_tu_db', // Tu nombre de base de datos MySQL
-  synchronize: false, // ¡Cuidado con esto en producción! Si es `true`, TypeORM intentará sincronizar el esquema de la base de datos con tus entidades, lo que puede causar pérdida de datos. Asume que tus tablas ya están creadas.
-  logging: false, // Puedes cambiar a true para ver los logs de SQL
-  entities: [Usuario, Area, Rol], // Aquí registras tus entidades (modelos)
-  migrations: [],
-  subscribers: [],
-});
-
-export const connectToDatabase = async () => {
-  try {
-    await dataSource.initialize();
-    console.log('Conexión a la base de datos MySQL establecida correctamente con TypeORM');
-    return dataSource; // Devuelve la instancia del DataSource si es exitoso
-  } catch (error) {
-    console.error('Error al conectar con la base de datos:', error);
-    // Aquí podrías relanzar el error o manejarlo de otra forma
-    throw error;
-  }
-};
+export const db = new Sequelize(process.env.DB_URL, {
+  models: [__dirname + '/../models/**/*'],
+  logging: false,
+  dialect: 'mysql'
+})
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,28 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import router from "./router";
+import colors from 'colors'
+import express from 'express'
+import morgan from 'morgan'
+import { db } from './config/db'
+import authRouter from './routes/authRouter'
 
-import { corsConfig } from "./config/cors";
+async function connectDB() {
+  try {
+    await db.authenticate()
+    db.sync()
+    console.log(colors.blue.bold('Conexion existosa a la BD'))
+  } catch (error) {
+    // console.log(error)
+    console.log(colors.red.bold('Fallo la conexion a la BD'))
+  }
+}
 
+connectDB()
 
-const app = express();
+const app = express()
 
-app.use(cors(corsConfig))
+app.use(morgan('dev'))
 
-app.use(express.json());
+app.use(express.json())
 
-app.use('/', router)
+app.use('/api/auth', authRouter)
 
-export default app;
+export default app
