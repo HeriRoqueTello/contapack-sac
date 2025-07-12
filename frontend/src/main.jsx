ba33fd57d1deda12401c1abc091187b6e61ab37a
Heri Roque <heriroque.22@gmail.com>
2025-07-11 22:34:24 -0500

    feat: add Toaster component for improved user notifications

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,14 @@ import "./index.css";
 import Router from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <Router />
       <ReactQueryDevtools />
     </QueryClientProvider>
