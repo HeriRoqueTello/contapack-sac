be161dd7f04730d40afa255c84d31f91de8fd101
Heri Roque <heriroque.22@gmail.com>
2025-07-12 14:15:26 -0500

    feat: create ServiciosView component to display various service offerings

---
+++ frontend/src/views/ServiciosView.jsx
@@ -0,0 +1,21 @@
+import { DistributionService } from "@/components/home/servicios/DistributionService";
+import { PackagingService } from "@/components/home/servicios/PackagingService";
+import { ProcessingService } from "@/components/home/servicios/ProcessingService";
+import { QualityService } from "@/components/home/servicios/QualityService";
+import { ServicesCTA } from "@/components/home/servicios/ServicesCTA";
+import { ServicesHero } from "@/components/home/servicios/ServicesHero";
+import { ServicesOverview } from "@/components/home/servicios/ServicesOverview";
+
+export default function ServiciosView() {
+  return (
+    <div className="min-h-screen bg-white">
+      <ServicesHero />
+      <ServicesOverview />
+      <ProcessingService />
+      <QualityService />
+      <DistributionService />
+      <PackagingService />
+      <ServicesCTA />
+    </div>
+  );
+}
