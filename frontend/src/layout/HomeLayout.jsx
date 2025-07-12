f72ef6ba7d25286e5249a24585a8ab143ae62dd5
Heri Roque <heriroque.22@gmail.com>
2025-07-12 14:39:19 -0500

    fix: reduce top padding in main section of HomeLayout for better layout consistency

--- frontend/src/layout/HomeLayout.jsx
+++ frontend/src/layout/HomeLayout.jsx
@@ -6,7 +6,7 @@ export function HomeLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow pt-16">
+      <main className="flex-grow pt-8">
         <Outlet />
       </main>
       <Footer />
