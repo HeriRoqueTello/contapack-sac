54c756d89ee71433838f811d16e4ae3a23d75c3e
anthony <anthony.diaz.yacsahuanga@gmail.com>
2025-07-16 19:38:11 -0500

    feat: add fields for Lote registration and integrate with LoteView component

--- frontend/src/components/admin/produccion/fieldsProduccion.jsx
+++ frontend/src/components/admin/produccion/fieldsProduccion.jsx
@@ -1,6 +1,29 @@
 export const fields = [
-  {name: "numero de pallet", label: "Número de pallet", type: "text", required: true},
-  {name: "calibre", label: "Calibre", type: "text", required: true},
-  {name: "categoria", label: "Categoría", type: "text", required: true},
-  {name: "peso por empaque", label: "Peso por empaque", type: "text", required: true}
-]
+  {
+    name: "numero de pallet",
+    label: "Número de pallet",
+    type: "text",
+    required: true,
+  },
+  { name: "calibre", label: "Calibre", type: "text", required: true },
+  { name: "categoria", label: "Categoría", type: "text", required: true },
+  {
+    name: "tipo de empaque",
+    label: "Tipo de empaque",
+    type: "radioGroup",
+    required: true,
+    options: ["Cajas", "Canastillas"],
+  },
+  {
+    name: "peso por empaque",
+    label: "Peso por empaque",
+    type: "text",
+    required: true,
+  },
+  {
+    name: "cantidad de empaques",
+    label: "Cantidad de empaques",
+    type: "text",
+    required: true,
+  },
+];
