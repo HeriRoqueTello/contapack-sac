f26d7f33c0d4e30daf27fc102b3e3357f9ed95af
Heri Roque <heriroque.22@gmail.com>
2025-07-27 12:17:20 -0500

    feat: Add useSignIn hook for user authentication with error handling

---
+++ frontend/src/api/auth/queryLogin.js
@@ -0,0 +1,33 @@
+import { useAuthStore } from "@/store/user-store";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { login } from "../api";
+import { useNavigate } from "react-router";
+import { isAxiosError } from "axios";
+
+export function useSignIn() {
+  const navigate = useNavigate();
+
+  const { setToken } = useAuthStore();
+  return useMutation({
+    mutationFn: (user) => login(user),
+    onSuccess: (data) => {
+      console.log(data);
+
+      setToken({
+        token: data,
+      });
+
+      toast.success("Acceso concedido");
+      navigate("/admin");
+    },
+    onError: (error) => {
+      const errorMessage = error.message;
+      if (isAxiosError(error) && error.response) {
+        toast.error(error.message || "Error del servidor");
+      } else {
+        toast.error(errorMessage);
+      }
+    },
+  });
+}
